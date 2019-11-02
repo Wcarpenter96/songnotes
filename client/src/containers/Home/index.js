aaad76314b3347022aaaa14ddfc6f2e499b5a3f7
Weston Carpenter <wc.cc96@gmail.com>
2019-11-02 15:07:39 -0700

    templated components and containers

---
+++ client/src/containers/Home/index.js
@@ -0,0 +1,12 @@
+import React from "react";
+import { Jumbotron } from 'react-bootstrap'
+
+function Home() {
+    return (
+        <Jumbotron>
+            <h1>Home Page</h1>
+        </Jumbotron>
+    );
+}
+
+export default Home;
